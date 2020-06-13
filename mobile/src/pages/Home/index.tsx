37924352834355052cb43a162f37131c2bce3785
Murilo Prado <kruzprado@hotmail.com>
2020-06-13 09:35:11 -0300

    Header of home WIP

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,20 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Feather as Icon } from "@expo/vector-icons";
 import Constants from "expo-constants";
 import { useNavigation } from "@react-navigation/native";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Alert, Image } from "react-native";
+import * as Location from "expo-location";
+
+interface Address {
+  city: string;
+  street: string;
+  region: string;
+  country: string;
+  postalCode: string;
+  name: string;
+}
 
 const Login = () => {
   const navigation = useNavigation();
+
+  const [address, setAddress] = useState("");
 
   function handleNavigationBack() {
     navigation.goBack();
   }
 
+  useEffect(() => {
+    async function loadLocation() {
+      const { status } = await Location.requestPermissionsAsync();
+
+      if (status !== "granted") {
+        Alert.alert(
+          "Ops....",
+          "Precisamos de sua permissão para obter a sua localização"
+        );
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+
+      const { latitude, longitude } = location.coords;
+
+      const address: Address[] = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      console.log(address[0]);
+
+      setAddress(
+        address[0].city
+          ? address[0].city + ", " + address[0].region
+          : "Joinville, Santa Catarina"
+      );
+    }
+
+    loadLocation();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}></View>
+      <View style={styles.header}>
+        <Icon name="info" size={30} style={styles.infoIcon} />
 
-      <View style={styles.welcome}></View>
+        <Icon name="map-pin" size={15} style={styles.mapPinIcon} />
+        <Text style={styles.city}>{address}</Text>
+        <Image
+          source={require("../../assets/roberto.jpeg")}
+          style={{
+            width: 50,
+            height: 50,
+            borderRadius: 100,
+            marginLeft: 50,
+          }}
+        />
+      </View>
+
+      <View style={styles.welcome}>
+        <View style={styles.information}>
+          <Text style={styles.text}>Olá </Text>
+          <Text style={styles.textBold}>Roberto,</Text>
+        </View>
+        <Text style={styles.description}>
+          vamos juntos desbravar este Brasil
+        </Text>
+      </View>
 
       <View style={styles.containerButtons}></View>
 
@@ -33,7 +101,40 @@ const styles = StyleSheet.create({
     paddingHorizontal: 32,
     paddingTop: 20 + Constants.statusBarHeight,
   },
-  header: {},
+  header: {
+    flexDirection: "row",
+  },
+  infoIcon: {
+    color: "#EB5757",
+    shadowOffset: { width: 0, height: 5 },
+    shadowColor: "#EB5757",
+    shadowOpacity: 0.25,
+    shadowRadius: 2,
+  },
+  mapPinIcon: {
+    color: "#80A1C1",
+    marginLeft: 20,
+    marginTop: 1,
+  },
+  information: {
+    flexDirection: "row",
+  },
+  city: {
+    fontSize: 16,
+    fontFamily: "Roboto_400Regular",
+  },
+  text: {
+    fontSize: 32,
+    fontFamily: "Roboto_400Regular",
+  },
+  textBold: {
+    fontSize: 32,
+    fontFamily: "Roboto_700Bold",
+  },
+  description: {
+    fontSize: 16,
+    fontFamily: "Roboto_400Regular",
+  },
   welcome: {},
   containerButtons: {},
   button: {},
---
+++ mobile/src/pages/Login/index.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
+import {
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+
+const Login = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToHome() {
+    navigation.navigate("Home");
+  }
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <View style={styles.container}>
+        <View style={styles.main}>
+          <View style={styles.truckContainer}>
+            <Icon name="truck-fast" style={styles.truckIcon} size={38} />
+          </View>
+          <Text style={styles.title}>Caminhão amigo</Text>
+          <Text style={styles.description}>
+            Valorizando o caminhoneiro e sua trajetória
+          </Text>
+        </View>
+
+        <View style={styles.footer}>
+          <RectButton style={styles.button} onPress={handleNavigateToHome}>
+            <Text style={styles.buttonText}>Participar</Text>
+          </RectButton>
+          <Text style={styles.community}>Conhecer a comunidade</Text>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    backgroundColor: "#5C8960",
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  title: {
+    color: "#f5f5f5",
+    fontSize: 32,
+    fontFamily: "Roboto_700Bold",
+    maxWidth: 260,
+    marginTop: 20,
+    alignSelf: "center",
+  },
+
+  description: {
+    color: "#f5f5f5",
+    fontSize: 12,
+    fontFamily: "Roboto_400Regular",
+    maxWidth: 260,
+    lineHeight: 24,
+    alignSelf: "center",
+  },
+
+  truckContainer: {
+    backgroundColor: "#fff",
+    width: 80,
+    height: 80,
+    borderRadius: 100,
+    alignSelf: "center",
+  },
+
+  truckIcon: {
+    textAlign: "center",
+    marginTop: 20,
+    color: "rgba(0, 0, 0, 0.8)",
+  },
+
+  footer: {
+    marginTop: 10,
+    marginBottom: 80,
+  },
+
+  button: {
+    backgroundColor: "#EF5B5B",
+    height: 60,
+    flexDirection: "row",
+    borderRadius: 20,
+    overflow: "hidden",
+    alignItems: "center",
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: "center",
+    textAlign: "center",
+    color: "#FFFFFF",
+    fontFamily: "Roboto_500Medium",
+    fontSize: 24,
+  },
+
+  community: {
+    color: "#FFFFFF",
+    textAlign: "center",
+    marginTop: 10,
+    fontFamily: "Roboto_400Regular",
+  },
+});
