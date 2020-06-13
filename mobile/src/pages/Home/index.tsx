555e7384a2b07c901dd780bd1c4ed6154d6f3166
Murilo Prado <kruzprado@hotmail.com>
2020-06-13 07:46:10 -0300

    Navigation to Home screen

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,115 +1,42 @@
 import React from "react";
-import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
-import {
-  StyleSheet,
-  View,
-  Text,
-  KeyboardAvoidingView,
-  Platform,
-} from "react-native";
-import { RectButton } from "react-native-gesture-handler";
+import Constants from "expo-constants";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, View, Text } from "react-native";
 
-const Home = () => {
+const Login = () => {
+  const navigation = useNavigation();
+
+  function handleNavigationBack() {
+    navigation.goBack();
+  }
+
   return (
-    <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      behavior={Platform.OS === "ios" ? "padding" : undefined}
-    >
-      <View style={styles.container}>
-        <View style={styles.main}>
-          <View style={styles.truckContainer}>
-            <Icon name="truck-fast" style={styles.truckIcon} size={38} />
-          </View>
-          <Text style={styles.title}>Caminhão amigo</Text>
-          <Text style={styles.description}>
-            Valorizando o caminhoneiro e sua trajetória
-          </Text>
-        </View>
+    <View style={styles.container}>
+      <View style={styles.header}></View>
 
-        <View style={styles.footer}>
-          <RectButton style={styles.button} onPress={() => {}}>
-            <Text style={styles.buttonText}>Participar</Text>
-          </RectButton>
-          <Text style={styles.community}>Conhecer a comunidade</Text>
-        </View>
-      </View>
-    </KeyboardAvoidingView>
+      <View style={styles.welcome}></View>
+
+      <View style={styles.containerButtons}></View>
+
+      <View style={styles.community}></View>
+
+      <View style={styles.footer}></View>
+    </View>
   );
 };
 
-export default Home;
+export default Login;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 32,
-    backgroundColor: "#5C8960",
+    paddingHorizontal: 32,
+    paddingTop: 20 + Constants.statusBarHeight,
   },
-
-  main: {
-    flex: 1,
-    justifyContent: "center",
-  },
-
-  title: {
-    color: "#f5f5f5",
-    fontSize: 32,
-    fontFamily: "Ubuntu_700Bold",
-    maxWidth: 260,
-    marginTop: 20,
-    alignSelf: "center",
-  },
-
-  description: {
-    color: "#f5f5f5",
-    fontSize: 12,
-    fontFamily: "Roboto_400Regular",
-    maxWidth: 260,
-    lineHeight: 24,
-    alignSelf: "center",
-  },
-
-  truckContainer: {
-    backgroundColor: "#fff",
-    width: 80,
-    height: 80,
-    borderRadius: 100,
-    alignSelf: "center",
-  },
-
-  truckIcon: {
-    textAlign: "center",
-    marginTop: 20,
-    color: "rgba(0, 0, 0, 0.8)",
-  },
-
-  footer: {
-    marginTop: 10,
-    marginBottom: 80,
-  },
-
-  button: {
-    backgroundColor: "#EF5B5B",
-    height: 60,
-    flexDirection: "row",
-    borderRadius: 20,
-    overflow: "hidden",
-    alignItems: "center",
-  },
-
-  buttonText: {
-    flex: 1,
-    justifyContent: "center",
-    textAlign: "center",
-    color: "#FFFFFF",
-    fontFamily: "Roboto_500Medium",
-    fontSize: 24,
-  },
-
-  community: {
-    color: "#FFFFFF",
-    textAlign: "center",
-    marginTop: 10,
-    fontFamily: "Roboto_400Regular",
-  },
+  header: {},
+  welcome: {},
+  containerButtons: {},
+  button: {},
+  community: {},
+  footer: {},
 });
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import Login from "./pages/Login";
 import Home from "./pages/Home";
 
 const AppStack = createStackNavigator();
@@ -17,6 +18,7 @@ const Routes = () => {
           },
         }}
       >
+        <AppStack.Screen name="Login" component={Login} />
         <AppStack.Screen name="Home" component={Home} />
       </AppStack.Navigator>
     </NavigationContainer>
