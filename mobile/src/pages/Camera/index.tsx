986f5b0af390f2080a8531a624aa0d07592556b9
Murilo Prado <kruzprado@hotmail.com>
2020-06-13 15:04:45 -0300

    Features pictures

--- mobile/src/pages/Camera/index.tsx
+++ mobile/src/pages/Camera/index.tsx
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Camera as Cam } from "expo-camera";
 
 const Camera = () => {
   const [hasPermission, setHasPermission] = useState<Boolean>(false);
   const [type, setType] = useState(Cam.Constants.Type.back);
+
+  function handleSwitchCamera() {
+    setType(
+      type === Cam.Constants.Type.back
+        ? Cam.Constants.Type.front
+        : Cam.Constants.Type.back
+    );
+  }
+
+  function handleTakePicture() {
+    alert("Vou tirar em!!");
+  }
+
+  function handleTurnFlash() {
+    alert("vou ligar o flash");
+  }
 
   useEffect(() => {
     (async () => {
@@ -23,31 +40,29 @@ const Camera = () => {
   return (
     <View style={{ flex: 1 }}>
       <Cam style={{ flex: 1 }} type={type}>
-        <View
-          style={{
-            flex: 1,
-            backgroundColor: "transparent",
-            flexDirection: "row",
-          }}
-        >
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: "flex-end",
-              alignItems: "center",
-            }}
-            onPress={() => {
-              setType(
-                type === Cam.Constants.Type.back
-                  ? Cam.Constants.Type.front
-                  : Cam.Constants.Type.back
-              );
-            }}
-          >
-            <Text style={{ fontSize: 18, marginBottom: 10, color: "white" }}>
-              {" "}
-              Flip{" "}
-            </Text>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.button} onPress={handleTurnFlash}>
+            <MaterialIcons
+              name="flash-off"
+              size={50}
+              color="#F5F5F5"
+            ></MaterialIcons>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={handleTakePicture}>
+            <MaterialIcons
+              name="camera"
+              size={50}
+              color="#F5F5F5"
+            ></MaterialIcons>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.button} onPress={handleSwitchCamera}>
+            <MaterialIcons
+              name="switch-camera"
+              size={50}
+              color="#F5F5F5"
+            ></MaterialIcons>
           </TouchableOpacity>
         </View>
       </Cam>
@@ -56,3 +71,23 @@ const Camera = () => {
 };
 
 export default Camera;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    width: "auto",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  button: {
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    marginBottom: 10,
+    color: "white",
+  },
+});
