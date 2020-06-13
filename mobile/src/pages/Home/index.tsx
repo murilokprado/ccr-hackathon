20a23eba01b77ca6fe4b1bd2d006573f242d4b96
Murilo Prado <kruzprado@hotmail.com>
2020-06-12 22:10:50 -0300

    Layout da home

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React from "react";
+import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
 import {
   StyleSheet,
   View,
   Text,
   KeyboardAvoidingView,
-  Platform
-} from 'react-native';
+  Platform,
+} from "react-native";
+import { RectButton } from "react-native-gesture-handler";
 
 const Home = () => {
   return (
     <KeyboardAvoidingView
       style={{ flex: 1 }}
-      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
     >
       <View style={styles.container}>
         <View style={styles.main}>
-          <Text style={styles.title}>Meu Amigo Caminhoneiro</Text>
+          <View style={styles.truckContainer}>
+            <Icon name="truck-fast" style={styles.truckIcon} size={38} />
+          </View>
+          <Text style={styles.title}>Caminhão amigo</Text>
           <Text style={styles.description}>
-            Ajudando caminhoneiros a compartilhar o seu dia-a-dia!
+            Valorizando o caminhoneiro e sua trajetória
           </Text>
+        </View>
+
+        <View style={styles.footer}>
+          <RectButton style={styles.button} onPress={() => {}}>
+            <Text style={styles.buttonText}>Participar</Text>
+          </RectButton>
+          <Text style={styles.community}>Conhecer a comunidade</Text>
         </View>
       </View>
     </KeyboardAvoidingView>
@@ -30,37 +42,74 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 32
+    padding: 32,
+    backgroundColor: "#5C8960",
   },
 
   main: {
     flex: 1,
-    justifyContent: 'center'
+    justifyContent: "center",
   },
 
   title: {
-    color: '#e61919',
+    color: "#f5f5f5",
     fontSize: 32,
-    fontFamily: 'Ubuntu_700Bold',
+    fontFamily: "Ubuntu_700Bold",
     maxWidth: 260,
-    marginTop: 64
+    marginTop: 20,
+    alignSelf: "center",
   },
 
   description: {
-    color: '#6C6C80',
-    fontSize: 16,
-    marginTop: 16,
-    fontFamily: 'Roboto_400Regular',
+    color: "#f5f5f5",
+    fontSize: 12,
+    fontFamily: "Roboto_400Regular",
     maxWidth: 260,
-    lineHeight: 24
+    lineHeight: 24,
+    alignSelf: "center",
+  },
+
+  truckContainer: {
+    backgroundColor: "#fff",
+    width: 80,
+    height: 80,
+    borderRadius: 100,
+    alignSelf: "center",
+  },
+
+  truckIcon: {
+    textAlign: "center",
+    marginTop: 20,
+    color: "rgba(0, 0, 0, 0.8)",
+  },
+
+  footer: {
+    marginTop: 10,
+    marginBottom: 80,
+  },
+
+  button: {
+    backgroundColor: "#EF5B5B",
+    height: 60,
+    flexDirection: "row",
+    borderRadius: 20,
+    overflow: "hidden",
+    alignItems: "center",
   },
 
   buttonText: {
     flex: 1,
-    justifyContent: 'center',
-    textAlign: 'center',
-    color: '#FFF',
-    fontFamily: 'Roboto_500Medium',
-    fontSize: 16
-  }
+    justifyContent: "center",
+    textAlign: "center",
+    color: "#FFFFFF",
+    fontFamily: "Roboto_500Medium",
+    fontSize: 24,
+  },
+
+  community: {
+    color: "#FFFFFF",
+    textAlign: "center",
+    marginTop: 10,
+    fontFamily: "Roboto_400Regular",
+  },
 });
