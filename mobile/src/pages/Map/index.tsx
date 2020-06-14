f36fc580df6f41a4821c1f486d6d0ae437add690
Murilo Prado <kruzprado@hotmail.com>
2020-06-14 18:44:22 -0300

    Fix alert color

--- mobile/src/pages/Map/index.tsx
+++ mobile/src/pages/Map/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import Constants from 'expo-constants';
-import MapView, { Marker } from 'react-native-maps';
+import React, { useState, useEffect } from "react";
+import Constants from "expo-constants";
+import MapView, { Marker } from "react-native-maps";
 import {
   StyleSheet,
   View,
@@ -8,87 +8,87 @@ import {
   Text,
   TouchableOpacity,
   Alert,
-  Modal as RNModal
-} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { RectButton } from 'react-native-gesture-handler';
-import * as Location from 'expo-location';
+  Modal as RNModal,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { RectButton } from "react-native-gesture-handler";
+import * as Location from "expo-location";
 import {
   Feather as Icon,
   FontAwesome,
-  MaterialCommunityIcons
-} from '@expo/vector-icons';
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
-import Modal from './Modal';
+import Modal from "./Modal";
 
 const points = [
   {
     id: 1,
     latitude: -26.29688,
     longitude: -48.883398,
-    name: 'Pórtico Opa Bier',
-    image_url: 'https://picsum.photos/200'
+    name: "Pórtico Opa Bier",
+    image_url: "https://picsum.photos/200",
   },
   {
     id: 2,
     latitude: -26.298307,
     longitude: -48.883696,
-    name: 'Point 2',
-    image_url: 'https://picsum.photos/201'
+    name: "Point 2",
+    image_url: "https://picsum.photos/201",
   },
   {
     id: 3,
     latitude: -26.300714,
     longitude: -48.883991,
-    name: 'Point 3',
-    image_url: 'https://picsum.photos/202'
-  }
+    name: "Point 3",
+    image_url: "https://picsum.photos/202",
+  },
 ];
 
 let spots = {
-  icon: 'hotel',
-  description: 'Parada',
+  icon: "hotel",
+  description: "Parada",
   visible: false,
   stop: [
     {
-      image_url: require('../../assets/parada1.png'),
-      km: '5',
-      stars: 4
+      image_url: require("../../assets/parada1.png"),
+      km: "5",
+      stars: 4,
     },
     {
-      image_url: require('../../assets/parada2.png'),
-      km: '5',
-      stars: 4
+      image_url: require("../../assets/parada2.png"),
+      km: "5",
+      stars: 4,
     },
     {
-      image_url: require('../../assets/parada3.png'),
-      km: '5',
-      stars: 4
-    }
-  ]
+      image_url: require("../../assets/parada3.png"),
+      km: "5",
+      stars: 4,
+    },
+  ],
 };
 
 let spotsGas = {
-  icon: 'gas-station',
-  description: 'Abastecimento',
+  icon: "gas-station",
+  description: "Abastecimento",
   visible: false,
   stop: [
     {
-      image_url: require('../../assets/parada1.png'),
-      km: '5',
-      stars: 4
+      image_url: require("../../assets/parada1.png"),
+      km: "5",
+      stars: 4,
     },
     {
-      image_url: require('../../assets/parada2.png'),
-      km: '5',
-      stars: 4
+      image_url: require("../../assets/parada2.png"),
+      km: "5",
+      stars: 4,
     },
     {
-      image_url: require('../../assets/parada3.png'),
-      km: '5',
-      stars: 4
-    }
-  ]
+      image_url: require("../../assets/parada3.png"),
+      km: "5",
+      stars: 4,
+    },
+  ],
 };
 
 const Map = () => {
@@ -98,7 +98,7 @@ const Map = () => {
   const [visibleEmergency, setVisibleEmergency] = useState(false);
   const [initialPosition, setInitialPosition] = useState<[number, number]>([
     -26.298754,
-    -48.883742
+    -48.883742,
   ]);
 
   function handleNavigationBack() {
@@ -131,17 +131,17 @@ const Map = () => {
 
   function handleNavigateToBuilding() {
     handleEmergency();
-    navigation.navigate('Building');
+    navigation.navigate("Building");
   }
 
   useEffect(() => {
     async function loadPosition() {
       const { status } = await Location.requestPermissionsAsync();
 
-      if (status !== 'granted') {
+      if (status !== "granted") {
         Alert.alert(
-          'Ops....',
-          'Precisamos de sua permissão para obter a sua localização'
+          "Ops....",
+          "Precisamos de sua permissão para obter a sua localização"
         );
         return;
       }
@@ -188,7 +188,7 @@ const Map = () => {
         <RectButton
           style={[
             styles.button,
-            { backgroundColor: '#EF5B5B', alignItems: 'center' }
+            { backgroundColor: "#EF5B5B", alignItems: "center" },
           ]}
           onPress={handleEmergency}
         >
@@ -205,7 +205,7 @@ const Map = () => {
               latitude: initialPosition[0],
               longitude: initialPosition[1],
               latitudeDelta: 0.014,
-              longitudeDelta: 0.014
+              longitudeDelta: 0.014,
             }}
           >
             {points.map((point) => (
@@ -215,14 +215,14 @@ const Map = () => {
                 onPress={() => {}}
                 coordinate={{
                   latitude: point.latitude,
-                  longitude: point.longitude
+                  longitude: point.longitude,
                 }}
               >
                 <View style={styles.mapMarkerContainer}>
                   <Image
                     style={styles.mapMarkerImage}
                     source={{
-                      uri: point.image_url
+                      uri: point.image_url,
                     }}
                   />
                   <Text style={styles.mapMarkerTitle}>{point.name}</Text>
@@ -238,7 +238,7 @@ const Map = () => {
           <RectButton
             style={[
               styles.button,
-              { backgroundColor: '#5B7488', alignItems: 'center' }
+              { backgroundColor: "#5B7488", alignItems: "center" },
             ]}
             onPress={handleOpenModalStopGas}
           >
@@ -252,7 +252,7 @@ const Map = () => {
           <RectButton
             style={[
               styles.button,
-              { backgroundColor: '#FFBA49', alignItems: 'center' }
+              { backgroundColor: "#FFBA49", alignItems: "center" },
             ]}
             onPress={handleOpenModalStop}
           >
@@ -272,14 +272,14 @@ const Map = () => {
         <View style={styles.modalImage}>
           <View
             style={{
-              alignItems: 'center'
+              alignItems: "center",
             }}
           >
             <View style={styles.alertContainer}>
               <View style={styles.alert}>
                 <Image
                   style={styles.alertImage}
-                  source={require('../../assets/alert.png')}
+                  source={require("../../assets/alert.png")}
                 />
               </View>
             </View>
@@ -298,16 +298,16 @@ const Map = () => {
           <View style={styles.buttonAlertContainer}>
             <TouchableOpacity
               style={{
-                backgroundColor: '#FFF',
-                justifyContent: 'center'
+                backgroundColor: "#FFF",
+                justifyContent: "center",
               }}
               onPress={handleEmergency}
             >
               <Text
                 style={{
                   fontSize: 15,
-                  fontFamily: 'Roboto_400Regular',
-                  color: '#5B7488'
+                  fontFamily: "Roboto_400Regular",
+                  color: "#5B7488",
                 }}
               >
                 Cancelar
@@ -315,19 +315,19 @@ const Map = () => {
             </TouchableOpacity>
             <TouchableOpacity
               style={{
-                backgroundColor: '#80A1C1',
+                backgroundColor: "#80A1C1",
                 borderRadius: 12,
-                width: '70%',
-                paddingVertical: 8
+                width: "70%",
+                paddingVertical: 8,
               }}
               onPress={handleNavigateToBuilding}
             >
               <Text
                 style={{
                   fontSize: 16,
-                  fontFamily: 'Roboto_400Regular',
-                  color: '#FFF',
-                  textAlign: 'center'
+                  fontFamily: "Roboto_400Regular",
+                  color: "#FFF",
+                  textAlign: "center",
                 }}
               >
                 Ver caminhoneiros próximos
@@ -346,173 +346,173 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 0,
-    paddingTop: Constants.statusBarHeight
+    paddingTop: Constants.statusBarHeight,
   },
   mapContainer: {
     flex: 1,
-    width: '100%',
+    width: "100%",
     borderRadius: 0,
-    overflow: 'hidden'
+    overflow: "hidden",
   },
   map: {
-    width: '100%',
-    height: '100%'
+    width: "100%",
+    height: "100%",
   },
   mapMarker: {
     width: 90,
-    height: 80
+    height: 80,
   },
   mapMarkerContainer: {
     width: 90,
     height: 70,
-    backgroundColor: '#34CB79',
-    flexDirection: 'column',
+    backgroundColor: "#34CB79",
+    flexDirection: "column",
     borderRadius: 8,
-    overflow: 'hidden',
-    alignItems: 'center'
+    overflow: "hidden",
+    alignItems: "center",
   },
   mapMarkerImage: {
     width: 90,
     height: 45,
-    resizeMode: 'cover'
+    resizeMode: "cover",
   },
   mapMarkerTitle: {
     flex: 1,
-    fontFamily: 'Roboto_400Regular',
-    color: '#FFF',
+    fontFamily: "Roboto_400Regular",
+    color: "#FFF",
     fontSize: 13,
-    lineHeight: 23
+    lineHeight: 23,
   },
   button: {
-    flexDirection: 'row',
+    flexDirection: "row",
     width: 143,
     height: 55,
-    borderRadius: 12
+    borderRadius: 12,
   },
   buttonText: {
     marginLeft: 10,
     fontSize: 16,
-    fontFamily: 'Roboto_700Bold',
-    color: '#FFFFFF'
+    fontFamily: "Roboto_700Bold",
+    color: "#FFFFFF",
   },
   header: {
     zIndex: 1,
-    position: 'absolute',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '100%',
+    position: "absolute",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
     height: 70,
     marginTop: Constants.statusBarHeight,
     padding: 20,
-    backgroundColor: 'rgba(255, 255, 255, .7)',
-    borderRadius: 10
+    backgroundColor: "rgba(255, 255, 255, .7)",
+    borderRadius: 10,
   },
   headerBack: {},
   emergency: {
     paddingTop: 80 + Constants.statusBarHeight,
     zIndex: 1,
-    position: 'absolute',
-    alignSelf: 'center'
+    position: "absolute",
+    alignSelf: "center",
   },
   info: {
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   headerText: {
     fontSize: 16,
-    fontFamily: 'Roboto_400Regular',
-    color: '#445766',
-    alignSelf: 'center'
+    fontFamily: "Roboto_400Regular",
+    color: "#445766",
+    alignSelf: "center",
   },
   infoText: {
     marginLeft: 5,
-    fontFamily: 'Roboto_400Regular',
-    color: '#5B7488',
-    marginTop: 5
+    fontFamily: "Roboto_400Regular",
+    color: "#5B7488",
+    marginTop: 5,
   },
   icon: {
     fontSize: 15,
-    color: '#5B7488',
-    marginTop: 5
+    color: "#5B7488",
+    marginTop: 5,
   },
   footer: {
     zIndex: 1,
-    position: 'absolute',
-    flexDirection: 'row',
-    alignContent: 'flex-end',
-    justifyContent: 'space-between',
-    width: '100%',
+    position: "absolute",
+    flexDirection: "row",
+    alignContent: "flex-end",
+    justifyContent: "space-between",
+    width: "100%",
     height: 70,
     padding: 40,
     borderRadius: 10,
-    bottom: 50
+    bottom: 50,
   },
   buttonIcon: {
     fontSize: 25,
-    color: '#f5f5f5',
-    marginLeft: 20
+    color: "#f5f5f5",
+    marginLeft: 20,
   },
   modalImage: {
     flex: 1,
-    position: 'absolute',
-    alignContent: 'flex-end',
-    justifyContent: 'space-between',
-    backgroundColor: '#FFF',
-    width: '100%',
-    height: '40%',
+    position: "absolute",
+    alignContent: "flex-end",
+    justifyContent: "space-between",
+    backgroundColor: "#FFF",
+    width: "100%",
+    height: "40%",
     bottom: 0,
     borderTopLeftRadius: 32,
-    borderTopRightRadius: 32
+    borderTopRightRadius: 32,
   },
   alertContainer: {
-    width: '100%'
+    width: "100%",
   },
   alert: {
     marginTop: -50,
     width: 150,
     height: 150,
     borderRadius: 100,
-    backgroundColor: '#ccc',
-    flexDirection: 'row',
-    alignSelf: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#FFF",
+    flexDirection: "row",
+    alignSelf: "center",
+    justifyContent: "center",
   },
   alertImage: {
-    alignSelf: 'center',
+    alignSelf: "center",
     height: 100,
     width: 100,
-    justifyContent: 'center'
+    justifyContent: "center",
   },
   textAlertContainer: {
-    flexDirection: 'row',
-    alignSelf: 'center',
-    alignItems: 'center',
-    textAlign: 'center'
+    flexDirection: "row",
+    alignSelf: "center",
+    alignItems: "center",
+    textAlign: "center",
   },
   textAlert: {
     fontSize: 24,
-    fontFamily: 'Roboto_700Bold',
-    color: '#5B7488',
-    marginBottom: 10
+    fontFamily: "Roboto_700Bold",
+    color: "#5B7488",
+    marginBottom: 10,
   },
   descriptionAlertContainer: {
-    alignSelf: 'center',
-    width: '100%',
-    alignItems: 'center',
-    textAlign: 'center'
+    alignSelf: "center",
+    width: "100%",
+    alignItems: "center",
+    textAlign: "center",
   },
   descriptionAlert: {
     fontSize: 16,
-    fontFamily: 'Roboto_400Regular',
-    color: '#5B7488',
-    paddingHorizontal: 8
+    fontFamily: "Roboto_400Regular",
+    color: "#5B7488",
+    paddingHorizontal: 8,
   },
   buttonAlertContainer: {
-    position: 'absolute',
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-between',
+    position: "absolute",
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
     bottom: 0,
-    padding: 20
-  }
+    padding: 20,
+  },
 });
